d0930654b6c2ac4ecfef5228f85b1f2b30e5c6dc
Flc <four_leaf_clover@foxmail.com>
2016-09-18 23:16:44 +0800

    签名相关

--- src/Alidayu/Client.php
+++ src/Alidayu/Client.php
@@ -13,16 +13,22 @@ use Flc\Alidayu\Requests\IRequest;
 class Client
 {   
     /**
+     * API请求地址
+     * @var string
+     */
+    protected $api_uri = 'http://gw.api.taobao.com/router/rest';
+
+    /**
      * 应用
      * @var \Flc\Alidayu\App
      */
     protected $app;
 
     /**
-     * 阿里大于配置
-     * @var array
+     * 签名规则
+     * @var string
      */
-    protected $config = [];
+    protected $sign_method = 'md5';
 
     /**
      * 初始化
@@ -34,12 +40,80 @@ class Client
 
         // 判断配置
         if (empty($this->app->app_key) || empty($this->app->app_secret)) {
-            throw new Exception("阿里大于配置信息：app_key或app_secret错误");            
+            throw new Exception("阿里大于配置信息：app_key或app_secret错误");
         }
     }
 
     public function execute(IRequest $request)
     {
-        return $request->getParams();
+        return $this->generateSign($request->getParams());
+    }
+
+    /**
+     * 生成签名
+     * @param  array  $params 待签参数
+     * @return string         
+     */
+    protected function generateSign($params = [])
+    {
+        if ($this->sign_method == 'md5') {
+            return $this->generateMd5Sign($params);
+        }
+
+        elseif ($this->sign_method == 'hmac') {
+            return $this->generateHmacSign($params);
+        }
+
+        else {
+            throw new Exception("sign_method错误...");
+        }
+    }
+
+    /**
+     * 按Md5方式生成签名
+     * @param  array  $params 待签的参数
+     * @return string         
+     */
+    protected function generateMd5Sign($params = [])
+    {
+        static::sortParams($params);  // 排序
+
+        $arr = [];
+        foreach ($params as $k => $v) {
+            $arr[] = $k . $v;
+        }
+        
+        $str = $this->app->app_secret . implode('', $arr) . $this->app->app_secret;
+
+        return strtoupper(md5($str));
+    }
+
+    /**
+     * 按hmac方式生成签名
+     * @param  array  $params 待签的参数
+     * @return string         
+     */
+    protected function generateHmacSign($params = [])
+    {
+        static::sortParams($params);  // 排序
+
+        $arr = [];
+        foreach ($params as $k => $v) {
+            $arr[] = $k . $v;
+        }
+        
+        $str = implode('', $arr);
+
+        return strtoupper(hash_hmac('md5', $str, $this->app->app_secret));
+    }
+
+    /**
+     * 待签名参数排序
+     * @param  array  &$params 参数
+     * @return array         
+     */
+    protected static function sortParams(&$params = [])
+    {
+        ksort($params);
     }
 }
