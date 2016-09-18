4ba70c4054ef1ffb62015a6636dbe828339dca6c
Flc <four_leaf_clover@foxmail.com>
2016-09-18 19:45:53 +0800

    init

---
+++ src/Alidayu/Client.php
@@ -0,0 +1,26 @@
+<?php
+namespace Flc\Alidayu;
+
+/**
+ * 阿里大于客户端
+ *
+ * @author Flc <2016-09-18 19:43:18>
+ * @link   http://flc.ren
+ */
+class Client
+{   
+    /**
+     * 阿里大于配置
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * 初始化
+     * @param array $config 阿里大于配置
+     */
+    public function __construct($config = [])
+    {
+        
+    }
+}
---
+++ tests/index.php
@@ -0,0 +1,5 @@
+<?php
+use Flc\Alidayu\Client;
+
+require __DIR__ . '/../vendor/autoload.php';
+
