612074849883bd33ce035bd0f61f6a66287c87ad
Flc <four_leaf_clover@foxmail.com>
2016-09-20 21:47:48 +0800

    新增随机数生成

--- src/Alidayu/Support.php
+++ src/Alidayu/Support.php
@@ -28,4 +28,17 @@ class Support
 
         return '';
     }
+
+    /**
+     * 获取随机位数数字
+     * @param  integer $len 长度
+     * @return string       
+     */
+    public static function randStr($len = 6)
+    {
+        $chars = str_repeat('0123456789', $len);
+        $chars = str_shuffle($chars);
+        $str   = substr($chars, 0, $len);
+        return $str;
+    }
 }
