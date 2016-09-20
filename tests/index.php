5821dddea7fb05a07d58008170b5c45baf841f99
Flc <four_leaf_clover@foxmail.com>
2016-09-20 22:02:13 +0800

    docs

--- tests/index.php
+++ tests/index.php
@@ -15,8 +15,8 @@ require __DIR__ . '/../vendor/autoload.php';
 
 // 配置信息
 $config = [
-    'app_key'    => '23311747',
-    'app_secret' => '89bb459e858382d1aac4ae2ccc9885cb',
+    'app_key'    => '*****',
+    'app_secret' => '************',
 ];
 
 $client = new Client(new App($config));
